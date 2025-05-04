61c92eefdbcd923207c37e1e4ea31dbac49c6bae
Shaun <shaunpalbath@hotmail.sg>
2025-05-04 23:09:31 +0800

    fix temp file

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ with vid_caption:
     with st.form('video_upload'):
         video_file = st.file_uploader("Upload a video file", type=["mp4", "mov", "avi"], accept_multiple_files=False)
         os.makedirs(TEMP_FOLDER_DIR, exist_ok=True)
-        vid_temp = NamedTemporaryFile(dir=TEMP_FOLDER_DIR, delete=True, suffix='.mp4')
+        vid_temp = NamedTemporaryFile(dir=TEMP_FOLDER_DIR, delete=False, suffix='.mp4')
         submitted = st.form_submit_button("Upload")
         if submitted:
             if not video_file:
